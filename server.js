a4ced59213eb83bcc3c76346557924963d75474b
bug84151 <bug84151@gmail.com>
2024-05-09 21:33:32 +0100

    api test

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
     res.send("hello from node api")
 })
@@ -10,4 +12,17 @@ app.get("/blog", (req, res) => {
   res.send("hello blog, my name is bug");
 });
 
-app.listen(3000, ()=> console.log('server started on port 3000'))
+app.post("/product", (req, res) => {
+    console.log(req.body);
+    res.send(req.body)
+})
+
+mongoose.
+    connect(
+  "mongodb+srv://admin:admin@bugsapi.hwxkn37.mongodb.net/Node-Api?retryWrites=true&w=majority&appName=bugsApi"
+).then(() => {
+    console.log("connected to mongodb");
+    app.listen(3000, () => console.log("server started on port 3000"));
+}).catch((err) => {
+    console.log(err);
+})
