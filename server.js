8e5a15f19d7ed2d4d76ff848f206623184b3d196
bug84151 <bug84151@gmail.com>
2024-05-10 13:09:49 +0100

    api test

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
+const Product = require("./models/productModel")
+
+const app = express()
 
 app.use(express.json())
 
@@ -12,10 +14,24 @@ app.get("/blog", (req, res) => {
   res.send("hello blog, my name is bug");
 });
 
-app.post("/product", (req, res) => {
-    console.log(req.body);
-    res.send(req.body)
+app.post("/product", async (req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message:error.message})
+    }
 })
+
+app.get("/products", async (req, res) => {
+  try {
+    const product = await Product.find({})
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 mongoose.
     connect(
