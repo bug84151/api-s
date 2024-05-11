6406a80c1f5e08ebde57ffea1c78379bf8e2ea7b
bug84151 <bug84151@gmail.com>
2024-05-11 20:42:30 +0100

    api test

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const Product = require("./models/productModel")
 
 const app = express()
 
+// using json
 app.use(express.json())
+// using forms
+app.use(express.urlencoded({extended:false}))
 
 app.get("/", (req, res) => {
     res.send("hello from node api")
@@ -27,6 +30,47 @@ app.post("/product", async (req, res) => {
 app.get("/products", async (req, res) => {
   try {
     const product = await Product.find({})
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+})
+
+// update a product
+app.put("/products/:id", async (req,res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body)
+    if (!product) {
+      return res.status(404).json({message:`cannot find product with ID ${id}`})
+    }
+    const updatedProduct = await Product.findById(id)
+    res.status(200).json(updatedProduct)
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+})
+
+// delete product
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find product with ID ${id}` });
+    }
     res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: error.message });
